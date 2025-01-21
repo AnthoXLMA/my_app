86a7cb5de83416d3115471d53ba7ae1cea0be74b
Anthony Mania <amj2544@tutanota.com>
2025-01-21 15:20:00 +0100

    Yarn Last Committed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,10 +2,13 @@
 //= require_tree
 //= require bootstrap
 
-import $ from 'jquery';
-import "./datepicker";  // Ensure you import the datepicker script
-// import "@popperjs/core";  // Ensure Popper.js is available
+import "bootstrap"
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import $ from 'jquery'
+import "./datepicker"  // Ensure you import the datepicker script
+import "@popperjs/core"  // Ensure Popper.js is available
+import * as Popper from '@popperjs/core'
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Bonne Nouvelle, ça fonctionne!');
